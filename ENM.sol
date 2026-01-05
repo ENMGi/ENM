d8c06c5bd544f2714b8c2162ec8863ccf7c90c17
ENMGi <surfgeeksgo@gmail.com>
2026-01-05 14:32:03 -0500

    Update ENM.sol
    This Coin is only Mint  able by the owner, ENMGi aka Matthew Scott Gillis ******517, account holder and rights to the intellectual property of everything involving or has involved ENM that ENM could possibly take ownership of.

--- ENM.sol
+++ ENM.sol
@@ -1,19 +1,68 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
+// Compatible with OpenZeppelin Contracts ^5.5.0
+pragma solidity ^0.8.27;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import {ERC20BurnableUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import {ERC20PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PausableUpgradeable.sol";
+import {ERC20PermitUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+import {ERC20VotesUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import {NoncesUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/NoncesUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 
-contract ENMGi_Enterprise is ERC20, Ownable {
-    constructor() ERC20("ENMGi Enterprise", "ENM") Ownable(msg.sender) {
+/// @custom:security-contact surfgeeksgo@gmail.com
+contract ENMGiEnterprises is ERC20, Ownable, Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20PausableUpgradeable, OwnableUpgradeable, ERC20PermitUpgradeable, ERC20VotesUpgradeable, UUPSUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+     constructor() ERC20("ENMGiEnterprise", "ENM") Ownable(msg.sender){
         // Mint 10 Million ENM to your new wallet immediately
         _mint(msg.sender, 10000000 * 10 ** decimals());
     }
+    {
+        __ERC20_init("ENMGiEnterprises", "ENM");
+        __ERC20Burnable_init("ENMGiEnterprises", "ENM");
+        __ERC20Pausable_init("ENMGiEnterprises", "ENM");
+        __Ownable_init("ENMGiEnterprises", "ENM");
+        __ERC20Permit_init("ENMGiEnterprises", "ENM");
+        __ERC20Votes_init("ENMGiEnterprises", "ENM");
+    }
 
-    // The Function your Python Script calls to mint profit
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        override
+        onlyOwner
+    {}
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20Upgradeable, ERC20PausableUpgradeable, ERC20VotesUpgradeable)
+    {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner)
+        public
+        view
+        override(ERC20PermitUpgradeable, NoncesUpgradeable)
+        returns (uint256)
+    {
+        return super.nonces(owner);
+    }
 }
 
- 
